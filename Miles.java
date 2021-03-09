d9ac21a561ba3d55d4a25231f9b5df728c4c1390
AlexandraPyaterikova <AlexandraPyaterikova@yandex.ru>
2021-03-09 22:47:48 +0300

    code added

---
+++ Miles.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        long spend = 579;
+        int checkpoint = 20;
+
+        long TotalBonus = spend / checkpoint;
+        System.out.println(TotalBonus);
+    }
+}
